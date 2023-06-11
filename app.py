e6fadfc5d5fba2d069909e4e7aa211773f5dbf22
Sam7009 <samanthahussain7011@gmail.com>
2023-06-11 23:25:28 +0800

    best version

--- app.py
+++ app.py
@@ -2,35 +2,65 @@ from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
 import time
-from datetime import datetime
 
 app = Flask(__name__)
 
+# Create an empty list to store the chat history
+chat_history = []
+
 def get_word_definition(word):
-    timestamp = time.strftime('%H:%M')
-    web = requests.get('https://prpm.dbp.gov.my/Cari1?keyword=' + word)
-    data = web.content
-    soup = BeautifulSoup(data, features="html.parser")
-    tag = soup.find_all("div", "tab-pane fade in active")
-    definitions = [i.text for i in tag]
-    return definitions, timestamp
+    try:
+        timestamp = time.strftime('%H:%M')
+        web = requests.get('https://prpm.dbp.gov.my/Cari1?keyword=' + word)
+        data = web.content
+        soup = BeautifulSoup(data, features="html.parser")
+        tag = soup.find_all("div", "tab-pane fade in active")
+        definitions = [i.text for i in tag]
+        return definitions, timestamp
+    except Exception as e:
+        # Handle the error here, for example, print an error message
+        print(f"An error occurred: {str(e)}")
+        return None, None
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Add initial bot message to the chat history
+    if len(chat_history) == 0:
+        timestamp = time.strftime('%H:%M')
+        initial_bot_message = {
+            'content': 'Welcome to DictionaryBot! How can I assist you today?',
+            'sender': 'bot',
+            'timestamp': timestamp
+        }
+        chat_history.append(initial_bot_message)
+
+    return render_template('index.html', chat_history=chat_history)
 
 @app.route('/definition', methods=['POST'])
 def definition():
     word = request.form['word']
     definitions, timestamp = get_word_definition(word)
-    
-    if definitions:
-        show_thanks = request.form.get('answer') == 'yes'  
-        show_regret = request.form.get('answer') == 'no' 
-        return render_template('index.html', definitions=definitions, word=word, timestamp=timestamp, show_thanks=show_thanks, show_regret=show_regret)
-    else:
-        return render_template('index.html', word=word, error=True)
 
+    if definitions:
+        # Append user's message to the chat history
+        user_message = {'content': word, 'sender': 'user', 'timestamp': timestamp}
+        chat_history.append(user_message)
+
+        # Append bot's response to the chat history
+        bot_response = {'content': definitions, 'sender': 'bot', 'timestamp': timestamp}
+        chat_history.append(bot_response)
+
+        return render_template('index.html', chat_history=chat_history, timestamp=timestamp)
+    else:
+        # Append user's message to the chat history
+        user_message = {'content': word, 'sender': 'user', 'timestamp': timestamp}
+        chat_history.append(user_message)
+
+        # Append bot's response with error message to the chat history
+        bot_response = {'content': 'Sorry, I couldn\'t find a definition for that word.', 'sender': 'bot', 'timestamp': timestamp}
+        chat_history.append(bot_response)
+
+        return render_template('index.html', chat_history=chat_history, timestamp=timestamp, error=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,23 @@
 // Scroll to the bottom of the chat window
 function scrollChatWindowToBottom() {
     var chatWindow = document.querySelector('.chat-window');
-    chatWindow.scrollTop = chatWindow.scrollHeight;
-}
-
-// Execute when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
+    var lastMessage = chatWindow.lastElementChild;
+    lastMessage.scrollIntoView();
+  }
+  
+  // Execute when the DOM is fully loaded
+  document.addEventListener('DOMContentLoaded', function() {
     scrollChatWindowToBottom();
-});
-
-// Scroll to the bottom when a new message is added
-function handleNewMessage() {
+  });
+  
+  // Scroll to the bottom when a new message is added
+  function handleNewMessage() {
     scrollChatWindowToBottom();
-}
-
-// Execute when the window is resized
-window.addEventListener('resize', function() {
+    document.getElementById('word').focus(); // Keep the input field focused
+  }
+  
+  // Execute when the window is resized
+  window.addEventListener('resize', function() {
     scrollChatWindowToBottom();
-});
-
-function selectChoice(choice) {
-    document.getElementById("msg_input").value = choice;
-    document.getElementById("send_button").click();
-}
-
-
+  });
+  

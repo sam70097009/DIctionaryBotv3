c8f3fc2de04b30eeeeeb1a21e72c92bf550e775f
Sam7009 <samanthahussain7011@gmail.com>
2023-06-14 21:55:30 +0800

    init

--- static/js/main.js
+++ static/js/main.js
@@ -3,21 +3,31 @@ function scrollChatWindowToBottom() {
     var chatWindow = document.querySelector('.chat-window');
     var lastMessage = chatWindow.lastElementChild;
     lastMessage.scrollIntoView();
-  }
-  
-  // Execute when the DOM is fully loaded
-  document.addEventListener('DOMContentLoaded', function() {
+}
+
+// Execute when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
     scrollChatWindowToBottom();
-  });
-  
-  // Scroll to the bottom when a new message is added
-  function handleNewMessage() {
+});
+
+// Scroll to the bottom when a new message is added
+function handleNewMessage() {
     scrollChatWindowToBottom();
     document.getElementById('word').focus(); // Keep the input field focused
-  }
-  
-  // Execute when the window is resized
-  window.addEventListener('resize', function() {
+
+    // Add animation to the latest messages
+    var messages = document.querySelectorAll('.message');
+    var latestMessages = Array.from(messages).slice(-2); // Change the number to specify the number of latest messages to animate
+
+    latestMessages.forEach(function(message) {
+        message.classList.add('animated'); // Add your animation class here
+    });
+}
+
+// Execute when the window is resized
+window.addEventListener('resize', function() {
     scrollChatWindowToBottom();
-  });
-  
+});
+
+// Call the handleNewMessage function on page load to apply the animation to the initial messages
+handleNewMessage();
